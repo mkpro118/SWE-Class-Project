15ec18310cd5c832f96843d4df045c7aa77fa8d6
markuspriede <priede@wisc.edu>
2024-03-14 13:13:17 -0500

    Setup navigation between pages and main styling of website

---
+++ src/newfrontend/components/sidenav.jsx
@@ -0,0 +1,60 @@
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import BoeingLogo from "../app/icons/Boeing_full_logo.svg.png"
+import { usePathname } from 'next/navigation';
+import { SIDENAV_ITEMS } from '@/styles/constants';
+
+const SideNav = () => {
+  const [displaySelectedTab, setDisplaySelectedTab] = useState("Master Inventory");
+
+  return (
+    <div className="md:w-60 bg-slate-100 h-screen flex-1 fixed border-r-1 border-zinc-200 hidden md:flex top-0">
+      <div className="flex flex-col space-y-6 w-full">
+        <div
+          className="flex flex-row space-x-3 items-center justify-center md:justify-start md:px-6 bg-indigo-300 border-zinc-200 h-16 w-full"
+        >
+          <Link href="/">
+            <Image src={BoeingLogo} alt="Boeing Logo" priority={true} />
+          </Link>
+        </div>
+        <div
+          className="flex flex-row space-x-3 items-center justify-center md:justify-start md:px-6 border-b-2 border-zinc-200 h-12 w-full"
+        >
+          <span className="font-semibold text-xl hidden md:flex text-blue-800">{displaySelectedTab}</span>
+        </div>
+
+        <div className="flex flex-col space-y-2  md:px-6 ">
+          {SIDENAV_ITEMS.map((item, index) => {
+            return <MenuItem key={index} item={item} setDisplaySelectedTab={setDisplaySelectedTab} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
+
+const MenuItem = ({ item, setDisplaySelectedTab }) => {
+  const pathname = usePathname();
+  const handleClick = () => {
+    setDisplaySelectedTab(item.title);
+  };
+
+  return (
+    <div className="">
+      <Link
+        href={item.path}
+        onClick={handleClick}
+        className={`flex flex-row space-x-4 items-center p-2 rounded-lg hover:bg-zinc-200 ${item.path === pathname ? 'bg-zinc-200' : ''
+          }`}
+      >
+        {item.icon}
+        <span className="font-medium text-md flex text-slate-600">{item.title}</span>
+      </Link>
+    </div>
+  );
+};
