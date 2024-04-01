f0d468a9c4aaaf79e8bdcb3597f3f3775f1eb08a
markuspriede <priede@wisc.edu>
2024-04-01 15:52:03 -0500

    Added API/URL tests for frontend using Jest and Axios libraries

---
+++ src/newfrontend/tests/CallAPI/customer.test.js
@@ -0,0 +1,83 @@
+// tests/urlFetch.test.ts
+import axios from "axios";
+
+describe('URL Fetch **Customer** Tests', () => {
+  const BASE_URL = 'http://127.0.0.1:5000';
+
+  test('GET request to /customer', async () => {
+    const response = await axios.get(`${BASE_URL}/customer`);
+    const responseBody = await response.data;
+    expect(response.config.method).toBe("get")
+    expect(response.status).toBe(200);
+    expect(() => JSON.parse(JSON.stringify(responseBody))).not.toThrow();
+  });
+
+  test('POST request to /customer', async () => {
+    const response = await axios.post(`${BASE_URL}/customer`);
+    const responseBody = await response.data;
+    expect(response.config.method).toBe("post")
+    expect(response.status).toBe(200);
+    expect(responseBody).toBe('Success on "/customer" with method POST');
+  });
+
+  test('GET request to /customer/{ID}', async () => {
+    const id = Math.floor(Math.random() * (1000 - 1) + 1);
+    const response = await axios.get(`${BASE_URL}/customer/${id}`);
+    const responseBody = await response.data;
+    expect(response.config.method).toBe("get")
+    expect(response.status).toBe(200);
+    expect(() => JSON.parse(JSON.stringify(responseBody))).not.toThrow();
+  });
+
+  test('DELETE request to /customer/{ID}', async () => {
+    const id = Math.floor(Math.random() * (1000 - 1) + 1);
+    const response = await axios.delete(`${BASE_URL}/customer/${id}`);
+    const responseBody = await response.data;
+    expect(response.config.method).toBe("delete")
+    expect(response.status).toBe(200);
+    expect(responseBody).toBe(`Success on "/customer/{ID}" with method DELETE\ncustomer_id = ${id}`);
+  });
+
+
+  test('PUT error request to /customer/{ID}', async () => {
+    try {
+        const id = Math.floor(Math.random() * (1000 - 1) + 1);
+        await axios.put(`${BASE_URL}/customer/${id}`);
+    } catch (error) {
+      expect(error.config.method).toBe("put")
+        expect(error.response.status).toBe(405);
+    };
+  });
+
+  test('POST error request to /customer/{ID}', async () => {
+    try {
+        const id = Math.floor(Math.random() * (1000 - 1) + 1);
+        await axios.post(`${BASE_URL}/customer/${id}`);
+    } catch (error) {
+      expect(error.config.method).toBe("post")
+        expect(error.response.status).toBe(405);
+    };
+  });
+
+  test('PUT error request to /customer', async () => {
+    try {
+        const id = Math.floor(Math.random() * (1000 - 1) + 1);
+        await axios.put(`${BASE_URL}/customer`);
+    } catch (error) {
+      expect(error.config.method).toBe("put")
+        expect(error.response.status).toBe(405);
+    };
+  });
+
+  test('DELETE error request to /customer', async () => {
+    try {
+        const id = Math.floor(Math.random() * (1000 - 1) + 1);
+        await axios.delete(`${BASE_URL}/customer`);
+    } catch (error) {
+      expect(error.config.method).toBe("delete")
+        expect(error.response.status).toBe(405);
+    };
+  });
+
+});
+
