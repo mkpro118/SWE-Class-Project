b01bddf61b15ca42f2400dd4f5a95fb3136df352
Parker Johnson <pljohnson2@wisc.edu>
2024-04-16 15:52:49 -0500

    Create pagination for table

--- src/frontend/app/inventory/page.jsx
+++ src/frontend/app/inventory/page.jsx
@@ -9,36 +9,69 @@ const MasterInventory = () => {
   const [fullInventory, setFullInventory] = useState([]);
   const [componentData, setComponentData] = useState([]);
   const [airplaneData, setAirplaneData] = useState([])
+  const [pageActive, setPageActive] = useState(1);
   const host = process.env.WEBSERVER_HOST || 'localhost';
   const port = process.env.WEBSERVER_PORT || 5000;
   const url = `http://${host}:${port}`;
-  
+
   //Load in all the data from API into corresponding arrays
   useEffect(() => {
     fetch(`${url}/airplane`)
-    .then(res => res.json())
-    .then(
-      data => {
-        setAirplaneData(data)
-        console.log(data)
-      }
-    )
+      .then(res => res.json())
+      .then(
+        data => {
+          setAirplaneData(data)
+          console.log(data)
+        }
+      )
     fetch(`${url}/component`)
-    .then(res => res.json())
-    .then(
-      data => {
-        setComponentData(data)
-        console.log(data)
-      }
-    )
+      .then(res => res.json())
+      .then(
+        data => {
+          setComponentData(data)
+          console.log(data)
+        }
+      )
   }, [url]);
-  
+
   // Merge fetched data into one array at runtime
   useEffect(() => {
     if (airplaneData && componentData) {
       setFullInventory([...airplaneData, ...componentData])
     }
   }, [airplaneData, componentData]);
+
+  //Pagination helpers
+  let pages = []
+  const pageBuilder = () => {
+    const numPages = Math.ceil(fullInventory.length / 12);
+    
+    //Loop is used to determine an active page and push to pages array for navigation
+    for (let i = 1; i <= numPages; i++) {
+      if (pageActive === i) {
+        pages.push(
+        <a className="flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
+          aria-current="true"
+          key={i}
+          onClick={() => setPageActive(i)}>{i}</a>)
+      }
+      else {
+        pages.push(
+          <a className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            aria-current="false"
+            key={i}
+            onClick={() => setPageActive(i)}>{i}</a>)
+      }
+    }
+    return pages
+  }
+  const handleNextPage = () => {
+    setPageActive(prevPage => prevPage + 1);
+  }
+
+  const handlePreviousPage = () => {
+    setPageActive(prevPage => prevPage - 1);
+  }
 
   return (
     <>
@@ -80,7 +113,7 @@ const MasterInventory = () => {
         </div>
       </header>
       {/*Table header is not located in table component because it maps everytime leading to duplicate headers*/}
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-gray-100">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500">
           <thead className="text-xs text-gray-700 uppercase bg-gray-100">
             <tr>
@@ -111,7 +144,7 @@ const MasterInventory = () => {
           </thead>
           <tbody>
             {
-              fullInventory.map(data => {
+              fullInventory.slice(12 * (pageActive - 1), 12 * pageActive).map(data => {
                 return <Table key={data.ID}
                   city={data.city}
                   state={data.state}
@@ -125,6 +158,15 @@ const MasterInventory = () => {
           </tbody>
           {/*Navigation element here to ensure there isn't extra data displayed (maybe?) or make the table size static with the option to scroll*/}
         </table>
+        <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between mt-2 mb-2 ml-4">
+          <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+            <button onClick={handlePreviousPage} disabled={(pageActive === 1)} className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">Previous
+            </button>
+            {pageBuilder()}
+            <button onClick={handleNextPage} disabled={(pageActive === (pages.length))} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700">Next
+            </button>
+          </ul>
+        </nav>
       </div>
     </>
   )
