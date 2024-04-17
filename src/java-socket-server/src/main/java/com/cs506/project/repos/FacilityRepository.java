f6b87e46614635ef2dfe2c16f64981aab740bf46
Daniel Sun <dsun69@wisc.edu>
2024-04-16 22:45:07 -0500

    created facility repo and adjusted facility schem

---
+++ src/java-socket-server/src/main/java/com/cs506/project/repos/FacilityRepository.java
@@ -0,0 +1,127 @@
+package com.cs506.project.repos;
+
+import com.cs506.project.JDBCConnection;
+import com.cs506.project.interfaces.ISQLRepository;
+import com.cs506.project.schemas.FacilitySchema;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+public class FacilityRepository implements ISQLRepository<FacilitySchema>{
+    private Connection connection;
+
+    public FacilityRepository (JDBCConnection connection){
+        //run jdbcconnection to get connection
+    }
+
+    /**
+     *
+     *
+     * */
+    @Override
+    public List<FacilitySchema> getAllWithBasicDetails(int limit) throws SQLException {
+        return null;
+    }
+
+    /**
+     * Fetches all rows and columns in the Facility table.
+     *
+     * @return List of Facility Java Objects.
+     */
+    @Override
+    public List<FacilitySchema> getAllWithAllDetails(int limit) throws SQLException {
+        List<FacilitySchema> facilities = new ArrayList<>();
+        String query = "SELECT * FROM Airplane";
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+
+                FacilitySchema facilitySchema= new FacilitySchema();
+
+                facilitySchema.facilityId = resultSet.getInt("FacilityID");
+                facilitySchema.name = resultSet.getString("Name");
+                facilitySchema.city = resultSet.getString("city");
+                facilitySchema.state = resultSet.getString("state");
+                facilitySchema.description = resultSet.getString("description");
+                facilitySchema.componentsInProduction = resultSet.getInt("componentsInProduction");
+                facilitySchema.componentsCompleted= resultSet.getInt("componentsCompleted");
+                facilitySchema.modelsInProduction = resultSet.getInt("modelsInProduction");
+                facilitySchema.modelsCompleted = resultSet.getInt("modelsCompleted");
+                facilitySchema.employeeCount = resultSet.getInt("employeeCount");
+                facilitySchema.managerId = resultSet.getInt("managerID");
+
+                facilities.add(facilitySchema);
+            }
+        }
+
+        return facilities;
+    }
+
+    /**
+     * Fetches an all data for Facility based on the id passed.
+     *
+     * @param facilityId : Id of the Facility queried.
+     * @return Facility Java Object.
+     */
+    @Override
+    public List<FacilitySchema> getById(int facilityId) throws SQLException{
+        List<FacilitySchema> facility = new ArrayList<>();
+        String query = "SELECT * FROM Facility WHERE facilityId = " + facilityId;
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);){
+
+            while (resultSet.next()) {
+
+                FacilitySchema facilitySchema = new FacilitySchema();
+
+                facilitySchema.facilityId = resultSet.getInt("FacilityID");
+                facilitySchema.name = resultSet.getString("Name");
+                facilitySchema.city = resultSet.getString("city");
+                facilitySchema.state = resultSet.getString("state");
+                facilitySchema.description = resultSet.getString("description");
+                facilitySchema.componentsInProduction = resultSet.getInt("componentsInProduction");
+                facilitySchema.componentsCompleted= resultSet.getInt("componentsCompleted");
+                facilitySchema.modelsInProduction = resultSet.getInt("modelsInProduction");
+                facilitySchema.modelsCompleted = resultSet.getInt("modelsCompleted");
+                facilitySchema.employeeCount = resultSet.getInt("employeeCount");
+                facilitySchema.managerId = resultSet.getInt("managerID");
+
+                facility.add(facilitySchema);
+
+            }
+        }
+
+        return facility;
+    }
+
+    @Override
+    public List<FacilitySchema> handleCreateQuery(List<FacilitySchema> requestEntities) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<FacilitySchema> handleReadQuery(int limit, boolean readAllDetails, List<FacilitySchema> requestEntities) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<FacilitySchema> handleUpdateQuery(List<FacilitySchema> request) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<FacilitySchema> handleDeleteQuery(List<FacilitySchema> request) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void closeConnection() throws SQLException {
+
+    }
+
+
+}
