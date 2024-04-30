b40dae2e0d205b2125fd24183d9977295598c556
Markus Priede <mpriede19@gmail.com>
2024-04-29 21:47:04 -0500

    Revert "Add modal and delete back into facility page from merge conflict"
    This reverts commit 59f6061bc63a8ec10f7fb775a8d1d07f4c412a01.

--- src/frontend/app/facilities/page.jsx
+++ src/frontend/app/facilities/page.jsx
@@ -13,7 +13,6 @@ const Facilities = () => {
   const [facilities, setFacilities] = useState([]);
   const [componentData, setComponentData] = useState([]);
   const [airplaneData, setAirplaneData] = useState([]);
-  const [showModal, setShowModal] = useState(false);
 
 
   const mapFacilityToCard = (facility) => {
@@ -45,9 +44,6 @@ const Facilities = () => {
     console.log(facilities);
   }, [facilities]);
 
-  const handleOnClose = () => {
-    setShowModal(false);
-  }
   const handleDelete = (id) => {
     console.log(`Deleted item with ID: ${id}`);
 
@@ -109,9 +105,7 @@ const Facilities = () => {
                 props={facility}
                 airplaneData={airplaneData}
                 componentData={componentData}
-                onDelete={handleDelete}
                 key={facility.id}
-
             />
           })
         }
